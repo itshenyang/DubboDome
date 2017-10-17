5b13f600baa648610606328749e4ecd47f121a04
sy <769797226@qq.com>
2017-10-17 14:49:23 +0800

    sy

---
+++ dubbo_redis_client/src/main/java/com/keruis/client/controller/ClientController.java
@@ -0,0 +1,12 @@
+package com.keruis.client.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Administrator on 2017/8/24.
+ */
+@Controller
+public class ClientController {
+
+
+}
